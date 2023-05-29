a9aa8d2e9ef3a61cfc6dedff403df30b1321f6d3
Techyfun <info@shweb.me>
2023-05-29 12:12:57 +0600

    api added

---
+++ lib/constants.ts
@@ -0,0 +1,5 @@
+export const EXAMPLE_PATH = "Inforinteriors";
+export const CMS_NAME = "WordPress";
+export const CMS_URL = "https://wordpress.org";
+export const HOME_OG_IMAGE_URL =
+  "https://infointeriors.com/wp-content/uploads/2023/01/logo-2.png";
